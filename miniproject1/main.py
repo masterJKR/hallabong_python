3232f28b05801cbaa76aa2bed075f73c10a7c6f5
masterJKR <nilzero25@gamil.com>
2026-05-19 15:27:14 +0900

    5월 19일

--- miniproject1/main.py
+++ miniproject1/main.py
@@ -5,19 +5,20 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 from database.db import Base, engine
-from models import student
-from schemas import student_schema
-from routers import student_router
+from models import student , score
+from schemas import student_schema ,score_schema
+from routers import student_router ,score_router
 
 app = FastAPI()
 Base.metadata.create_all(bind=engine)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://localhost:5173" , ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
 app.include_router(student_router.router)
+app.include_router(score_router.router)
